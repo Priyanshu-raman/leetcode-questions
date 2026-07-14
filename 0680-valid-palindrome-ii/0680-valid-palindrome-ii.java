fd88456dc46d9eecaf242e535136089906b503f3
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-07-14 17:02:36 +0530

    Time: 9 ms (6.74%), Space: 48 MB (11.7%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -4,21 +4,41 @@ class Solution {
         int j=s.length()-1;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)){
-                return isPalindromeRange(s, i + 1, j) || isPalindromeRange(s, i, j - 1);
+                StringBuilder s1 = new StringBuilder(s);
+                s1.deleteCharAt(i);
+
+                StringBuilder s2 = new StringBuilder(s);
+                s2.deleteCharAt(j);
+                return isPalindrom(s1.toString()) || isPalindrom(s2.toString());
             }
             i++;
             j--;
         }
         return true;
     }
-    private boolean isPalindromeRange(String s, int i,int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+    private boolean isPalindrom(String s){
+        StringBuilder sb =new StringBuilder(s);
+        return s.equals(sb.reverse().toString());
     }
+    //     int i=0;
+    //     int j=s.length()-1;
+    //     while(i<j){
+    //         if(s.charAt(i)!=s.charAt(j)){
+    //             return isPalindromeRange(s, i + 1, j) || isPalindromeRange(s, i, j - 1);
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    // private boolean isPalindromeRange(String s, int i,int j){
+    //     while(i<j){
+    //         if(s.charAt(i)!=s.charAt(j)){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
 }
