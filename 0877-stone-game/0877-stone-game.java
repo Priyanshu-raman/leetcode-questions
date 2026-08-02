8609fd8b3c1f2310abab473fd32cfa3be0bdc933
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-08-02 16:49:18 +0530

    Time: 0 ms (100%), Space: 43 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        // return true;
+        int sum=0;
+        int sum1=0;
+        for(int i=0;i<piles.length;i++){
+            if(i%2==0){
+                sum+=piles[i];
+            }
+            else{
+                sum1+=piles[i];
+            }
+        }
+        if(sum>sum1){
+            return true;
+        }
+        else return true;
     }
 }
