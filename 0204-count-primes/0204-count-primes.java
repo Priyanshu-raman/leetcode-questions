b80bcc73693f6f93f29217b1d89ec54c4903596e
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-08-04 03:01:13 +0530

    Time: 54 ms (98.59%), Space: 46.2 MB (99.22%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,32 +1,32 @@
-class Solution {
+public class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) {
-            return 0; // No primes strictly less than 2
-        }
+        if (n <= 2) return 0;
+        
+        // We only track odd numbers: 3, 5, 7, 9, ...
+        // Size required is roughly n / 2
+        int HALF = n / 2;
+        boolean[] isComposite = new boolean[HALF];
+        
+        // Count starts at 1 to include prime number '2'
+        int count = 1; 
 
-        boolean[] isPrime = new boolean[n];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        // Mark composite numbers up to sqrt(n)
-        for (int p = 2; p * p < n; p++) {
-            if (isPrime[p]) {
-                // Start marking from p * p
-                for (int i = p * p; i < n; i += p) {
-                    isPrime[i] = false;
+        int sqrtN = (int) Math.sqrt(n);
+        
+        // i represents odd numbers (2*i + 1)
+        for (int i = 1; 2 * i + 1 < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                int prime = 2 * i + 1;
+                
+                // Mark multiples of this odd prime starting from prime^2
+                if (prime <= sqrtN) {
+                    for (int j = 2 * i * (i + 1); j < HALF; j += prime) {
+                        isComposite[j] = true;
+                    }
                 }
             }
         }
-
-        // Count remaining prime numbers
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (isPrime[i]) {
-                count++;
-            }
-        }
-
+        
         return count;
     }
 }
