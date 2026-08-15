569fa7896a2a0fc2d48ef331988523c60f2c2795
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-08-15 14:39:45 +0530

    Time: 83 ms (94.84%), Space: 51.2 MB (54.32%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,32 +1,51 @@
 public class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
+    //     if (n <= 2) return 0;
         
-        // We only track odd numbers: 3, 5, 7, 9, ...
-        // Size required is roughly n / 2
-        int HALF = n / 2;
-        boolean[] isComposite = new boolean[HALF];
+    //     // We only track odd numbers: 3, 5, 7, 9, ...
+    //     // Size required is roughly n / 2
+    //     int HALF = n / 2;
+    //     boolean[] isComposite = new boolean[HALF];
         
-        // Count starts at 1 to include prime number '2'
-        int count = 1; 
+    //     // Count starts at 1 to include prime number '2'
+    //     int count = 1; 
 
-        int sqrtN = (int) Math.sqrt(n);
+    //     int sqrtN = (int) Math.sqrt(n);
         
-        // i represents odd numbers (2*i + 1)
-        for (int i = 1; 2 * i + 1 < n; i++) {
-            if (!isComposite[i]) {
-                count++;
-                int prime = 2 * i + 1;
+    //     // i represents odd numbers (2*i + 1)
+    //     for (int i = 1; 2 * i + 1 < n; i++) {
+    //         if (!isComposite[i]) {
+    //             count++;
+    //             int prime = 2 * i + 1;
                 
-                // Mark multiples of this odd prime starting from prime^2
-                if (prime <= sqrtN) {
-                    for (int j = 2 * i * (i + 1); j < HALF; j += prime) {
-                        isComposite[j] = true;
-                    }
+    //             // Mark multiples of this odd prime starting from prime^2
+    //             if (prime <= sqrtN) {
+    //                 for (int j = 2 * i * (i + 1); j < HALF; j += prime) {
+    //                     isComposite[j] = true;
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return count;
+
+    if(n<2) return 0;
+    boolean[] q = new boolean[n];
+        int count = 0;
+
+        for (int i = 2; i * i < n; i++) {
+            if (!q[i]) { 
+                for (int j = i * i; j < n; j += i) {
+                    q[j] = true;
                 }
             }
         }
-        
+        for (int i = 2; i < n; i++) {
+            if (!q[i]) {
+                count++;
+            }
+        }
+
         return count;
     }
 }
