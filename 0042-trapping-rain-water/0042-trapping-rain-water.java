7447a705c526879e78da3112de88b4c5cb0b7725
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-08-27 01:47:41 +0530

    Time: 0 ms (100%), Space: 47.8 MB (55.57%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,49 +2,24 @@ class Solution {
     public int trap(int[] height) {
         int i=0;
         int j=height.length-1;
-        int max=height[i];
+        int imax=height[i];
+        int jmax=height[j];
         int sum=0;
-        int f=height[j];
-        
         while(i<j){
-            if(max<f){
+            if(imax<jmax){
                 i++;
-                max=Math.max(max,height[i]);
-                sum+=max-height[i];
-               
+                imax=Math.max(imax,height[i]);
+                sum+=imax-height[i];
             }else{
                 j--;
-                f=Math.max(f,height[j]);
-                sum+=f-height[j];
-                
+                jmax=Math.max(jmax,height[j]);
+                sum+=jmax-height[j];
             }
         }
         return sum;
     }
 }
 
-// class Solution {
-//     public int trap(int[] height) {
-//         if (height == null || height.length == 0) return 0;
-
-//         int left = 0, right = height.length - 1;
-//         int leftMax = height[left], rightMax = height[right];
-//         int water = 0;
-
-//         while (left < right) {
-//             if (leftMax < rightMax) {
-//                 left++;
-//                 leftMax = Math.max(leftMax, height[left]);
-//                 water += leftMax - height[left];
-//             } else {
-//                 right--;
-//                 rightMax = Math.max(rightMax, height[right]);
-//                 water += rightMax - height[right];
-//             }
-//         }
-//         return water;
-//     }
-// }
 
 
 
@@ -55,30 +30,3 @@ class Solution {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(height[i]>max){
-            //     int y=height[i]-max;
-            //     sum
-            //     max=height[i];
-            // }
-            // else if(height[i]<max){
-            //     if(height[i-1]>=height[i]){
-            //         sum+=height[i-1]-height[i];
-            //     }
-            //     else{
-            //         sum+=height[i]-height[i-1];
-            //     }
-            // }  
