1650d11f9f0d56668493236f193ff3b969edfb32
Priyanshu-raman <ramanpriyanshu08@gmail.com>
2026-09-11 00:56:18 +0530

    Time: 2 ms (56.95%), Space: 46.2 MB (79.2%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canConstruct(String r, String m) {
-        int[] freq = new int[26];
-        int[] freq1 = new int[26];
-        for(int i=0;i<r.length();i++){
-            char ch1=r.charAt(i);
-            freq[ch1-'a']++;
-        }
-        for(int i=0;i<m.length();i++){
+        int w=r.length();
+        int y=m.length();
+        int freq[] = new int[26];
+        for(int i=0;i<y;i++){
             char ch=m.charAt(i);
-            freq1[ch-'a']++;
+            freq[ch-'a']++;
         }
-        for(int i=0;i<26;i++){
-            freq[i]=freq[i]-freq1[i];
-            if(freq[i]>0){
+        for(int i=0;i<w;i++){
+            char ch=r.charAt(i);
+            freq[ch-'a']--;
+        }
+        for(int i=0;i<r.length();i++){
+            if(freq[r.charAt(i)-'a']<0){
                 return false;
             }
         }
